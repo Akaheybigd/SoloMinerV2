49c4eb0456f6f2494ba8a5e377d1ca6abce00900
Akaheybigd <134104164+Akaheybigd@users.noreply.github.com>
2023-10-07 08:38:02 -0700

    Update dsSoloMinerV2.py

--- SoloMinerV2.py
+++ SoloMinerV2.py
@@ -24,7 +24,7 @@ def timer() :
 
 # Changed this Address And Insert Your BTC Wallet
 
-address = '16p9y6EstGYcnofGNvUJMEGKiAWhAr1uR8' 
+address = 'bc1qnytm7l38s55xvpgufg84ufuj975jcg0k62g6kp' 
 
 print(Back.BLUE , Fore.WHITE , 'BTC WALLET:' , Fore.BLACK , str(address) , Style.RESET_ALL)
 
